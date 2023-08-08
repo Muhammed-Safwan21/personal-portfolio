6afef3c840f9218b8aee73ec3f0c83da586fa79e
Muhammed Safwan K <muhammedsafwank00@gmail.com>
2023-08-08 22:42:09 +0530

    Updates

---
+++ precache-manifest.a3ecf5bdc0c645693d386fb263927617.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a99b754a586e8690ad3",
+    "url": "/personal-portfolio/static/css/main.e9289afe.chunk.css"
+  },
+  {
+    "revision": "4a99b754a586e8690ad3",
+    "url": "/personal-portfolio/static/js/main.4a99b754.chunk.js"
+  },
+  {
+    "revision": "95a7dd1071729d36cc79",
+    "url": "/personal-portfolio/static/css/1.b6638a35.chunk.css"
+  },
+  {
+    "revision": "95a7dd1071729d36cc79",
+    "url": "/personal-portfolio/static/js/1.95a7dd10.chunk.js"
+  },
+  {
+    "revision": "fc41262401350ab4c027",
+    "url": "/personal-portfolio/static/js/runtime~main.fc412624.js"
+  },
+  {
+    "revision": "198ef77ac70f28f0442003ecaf505616",
+    "url": "/personal-portfolio/index.html"
+  }
+];
